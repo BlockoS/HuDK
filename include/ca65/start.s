e33e4a78f7f1a6a348e805cfc53c7fa72a5184b2
freem <aj187@ajworld.net>
2016-03-02 10:06:03 -0600

    ca65 version of start.s makes an assumption that there will be a segment called CODE with code in the fixed bank

---
+++ include/ca65/start.s
@@ -0,0 +1,24 @@
+  .ifndef CDROM
+    ; IRQ vectors (HuCard only)
+    .segment "VECTORS"
+    .word _irq_2
+    .word _irq_1
+    .word _timer
+    .word _nmi
+    .word _reset
+
+    .code
+    .include "irq_reset.s"
+    .include "irq_nmi.s"
+    .include "irq_timer.s"
+    .include "irq_1.s"
+    .include "irq_2.s"
+
+    .include "utils.s"
+    .include "psg.s"
+    .include "vdc.s"
+    .include "vce.s"
+    .include "font.s"
+  .else
+    ; [todo]
+  .endif ; !(CDROM)
