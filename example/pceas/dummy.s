8647644e5510aec7fa148a0d9cff6e10d893a086
MooZ <cruz.vincent@gmail.com>
2016-03-16 22:25:05 +0100

    Updated dummy example

--- example/pceas/dummy.s
+++ example/pceas/dummy.s
@@ -1,4 +1,5 @@
     .include "hudk.s"
+    .include "vgm.s"
 main:
     lda    #.bank(hudk.bitmap)
     sta    <_bl
@@ -68,9 +69,80 @@ main:
  
     ; enable background display
     vdc_reg  #VDC_CR
-    vdc_data #(VDC_CR_BG_ENABLE)
+    vdc_data #(VDC_CR_BG_ENABLE | VDC_CR_VBLANK_ENABLE)
+    
+;    lda #low(datastorm_base_address)
+;    sta <vgm_base
+;    sta <vgm_ptr
 
-    bra    *
+;    lda #high(datastorm_base_address)
+;    sta <vgm_base+1
+;    sta <vgm_ptr+1
+
+;    lda #datastorm_bank
+;    sta <vgm_bank
+
+;    lda #datastorm_loop_bank
+;    sta <vgm_loop_bank
+
+;    stw #datastorm_loop, <vgm_loop_ptr
+    
+;    lda <vgm_base+1
+;    clc
+;    adc #$20
+;    sta <vgm_end
+
+    smb0 <map_wrap
+
+    lda <font_base
+    sta <map_tile_base
+    lda <font_base+1
+    and #$0f
+    sta <map_tile_base+1
+
+    lda #bank(tilemap)
+    sta <map_bank
+    stw #(tilemap), <map_address
+
+    lda #bank(tile_palettes)
+    sta <map_pal_bank
+    stw #tile_palettes, <map_pal_address
+
+    stw #05, <map_width
+    stw #07, <map_height
+
+    lda #00
+    sta <map_bat_top
+    lda #32
+    sta <map_bat_bottom
+    
+    clx
+    lda <map_bat_top
+    jsr vdc_calc_addr
+    stw <_di, <map_bat_top_base
+
+    lda #21
+    sta <_al
+    lda #17
+    sta <_ah
+    lda #$00
+    sta <_cl
+    lda #$00
+    sta <_ch
+    lda #$07
+    sta <_dl
+    lda #$07
+    sta <_dh
+    jsr map_load
+ 
+.loop:
+    vdc_reg  #VDC_CR
+    lda    vdc_data_l
+    bit    #VDC_STATUS_VBLANK
+    beq    .nop
+; [todo]        jsr    vgm_update
+.nop
+    bra    .loop    
 
 ascii_string:
     .db "Lorem ipsum dolor sit amet, consectetuer adipiscing elit."
@@ -88,4 +160,23 @@ hudk.bitmap:
     .incbin "data/hudk.dat"
 hudk.bitmap.end:
 
+tilemap:
+    .db $01, $02, $03, $04, $05
+    .db $11, $12, $13, $14, $15
+    .db $21, $22, $23, $24, $25
+    .db $31, $32, $33, $34, $35
+    .db $06, $07, $08, $09, $0A
+    .db $16, $17, $18, $19, $1A
+    .db $26, $27, $28, $29, $2A
+
+tile_palettes:
+    .db $10,$00,$00,$00,$10,$10,$00,$00,$00,$10,$10,$10,$10,$10,$10,$10
+    .db $10,$00,$10,$00,$10,$10,$00,$10,$10,$10,$10,$10,$10,$10,$10,$10
+    .db $10,$00,$00,$00,$10,$10,$00,$00,$00,$10,$10,$10,$10,$10,$10,$10
+    .db $10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10
+
     .include "font.inc"
+
+; [todo] vgm data
+;    .data
+;    .include "data/datastorm.inc"
