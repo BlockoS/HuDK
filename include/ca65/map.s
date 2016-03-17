24315ea33a41a92a04c5ca172717a0bc5687fb54
MooZ <cruz.vincent@gmail.com>
2016-03-17 18:41:08 +0100

    Added ca65 macros for map helpers.

---
+++ include/ca65/map.s
@@ -0,0 +1,31 @@
+; Set current map pointers and infos.
+  .macro map_set map, tile, colortab, w, h, m
+    lda    #.bank(map)
+    sta    <map_bank
+    stw    #map, <map_address
+    stw    #((tile)>>4), <map_tile_base
+    lda    #.bank(colortab)
+    sta    <map_pal_bank
+    stw    #colortab, <map_pal_address
+    stw    w, <map_width
+    stw    h, <map_height
+    lda    m
+    sta    <map_wrap
+  .endmacro
+
+; Map copy helper macro.
+  .macro map_copy bx, by, mx, my, w, h
+    lda    bx
+    sta    <_al
+    lda    by
+    sta    <_ah
+    lda    mx
+    sta    <_cl
+    lda    my
+    sta    <_ch
+    lda    w
+    sta    <_dl
+    lda    h
+    sta    <_dh
+    jsr    map_load
+  .endmacro
