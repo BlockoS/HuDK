57d4492651bf7ff402acf9bbafd09f391aee615e
MooZ <cruz.vincent@gmail.com>
2016-04-19 22:47:06 +0200

    Started working on a simple BRAM tool.

---
+++ example/pceas/bm_tools.s
@@ -0,0 +1,73 @@
+    .include "hudk.s"
+    .include "bram.s"
+
+    .code
+main:
+    ; load default font
+    stw    #$2000, <_di 
+    lda    #.bank(font_8x8)
+    sta    <_bl
+    stw    #font_8x8, <_si
+    stw    #(FONT_8x8_COUNT*8), <_cx
+    jsr    font_load
+
+    lda    #$00
+    jsr    font_set_pal
+
+    ; load palette
+    stw    #palette, <_si
+    jsr    map_data
+    cla
+    ldy    #$02
+    jsr    vce_load_palette
+    
+    ; fill BAT with space character
+    lda    #' '
+    sta    <_bl
+    lda    vdc_bat_width
+    sta    <_al
+    lda    vdc_bat_height
+    sta    <_ah
+    ldx    #$00
+    lda    #$00
+    jsr    print_fill
+
+    ; detect BRAM
+    jsr    bm_detect
+    ldx    bm_error
+    lda    bm_detect_msg.lo, X
+    sta    <_si
+    lda    bm_detect_msg.hi, X
+    sta    <_si+1
+    lda    #32              ; [todo]
+    sta    <_al
+    lda    #32              ; [todo]
+    sta    <_ah
+    ldx    #7               ; [todo]
+    lda    #1               ; [todo]
+    jsr    print_string
+    
+    ; enable background display
+    vdc_reg  #VDC_CR
+    vdc_data #(VDC_CR_BG_ENABLE)
+    
+    cli 
+.loop:
+    nop
+    bra    .loop    
+
+bm_detect_msg.lo:
+    .dwl bm_detect_msg00, bm_detect_msg01, bm_detect_msg02
+bm_detect_msg.hi:
+    .dwh bm_detect_msg00, bm_detect_msg01, bm_detect_msg02
+
+bm_detect_msg00: .db "backup ram detected", 0
+bm_detect_msg01: .db "backup ram not found", 0
+bm_detect_msg02: .db "backup ram is not formated", 0
+
+palette:
+    .db $00,$00,$ff,$01,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+    .db $00,$00,$00,$01,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+
+    .include "font.inc"
+
