7a1d3e214cd43b06f9c2b338466e48c72ba14c46
MooZ <cruz.vincent@gmail.com>
2016-04-25 22:18:34 +0200

    Started porting magicKit irq_1 handler.

--- include/irq_1.s
+++ include/irq_1.s
@@ -1,6 +1,53 @@
 _irq_1:
     bbs1   <irq_m, @user_hook
-    ; [todo]
+
+    pha                     ; save registers
+    phx
+    phy
+
+    lda    video_reg        ; get VDC status register
+    sta    <vdc_sr
+
+@vsync:                     ; vsync interrupt
+    bbr5   <vdc_sr, @hsync
+
+; [todo]    inc    irq_cnt          ; update irq counter (for wait_vsync)
+
+    st0    #$05             ; update display control (bg/sp)
+    lda    <vdc_crl         ; vdc control register
+    sta    video_data_l
+    lda    <vdc_crh
+    sta    video_data_h
+
+    bbs5   <irq_m, @l3
+; [todo]        jsr  _vsync_hndl
+@l3:
+    bbr4   <irq_m, @l4
+        jsr  @user_vsync
+@l4:
+
+@hsync:
+    bbr2   <vdc_sr, @exit
+    bbs7   <irq_m,  @l5
+
+; [todo]    jsr  _hsync_hndl
+
+@l5:
+    bbr6 <irq_m, @exit
+        jsr  @user_hsync
+
+@exit:
+    lda    <vdc_reg         ; restore VDC register index
+    sta    video_reg
+
+    ply                     ; restore registers
+    plx
+    pla
+
     rti
 @user_hook:
     jmp    [irq1_hook]
+@user_hsync:
+    jmp    [hsync_hook]
+@user_vsync:
+    jmp    [vsync_hook]
