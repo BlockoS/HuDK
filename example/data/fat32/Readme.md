80a796f5b4922a9a02c43a0a787334021e134356
mooz <mooz@blockos.org>
2018-08-31 21:28:36 +0200

    Adds FAT32 data files.

---
+++ example/data/fat32/Readme.md
@@ -0,0 +1,56 @@
+## How to recreate the fat32 files used for the fake SD card 
+
+### Regenerate the fat32 image
+- `dd if=/dev/zero of=/tmp/fat32.img bs=1M count=40`
+- `sync`
+- `sudo losetup loop0 fat32.img`
+- `sudo -H gparted /dev/loop0`
+  - create a msdos partition table
+  - create a fat32 partition
+  - don't forget to apply changes
+  - quit
+ - `mkdir /tmp/fat32.0`
+- `sudo mount /dev/loop0p1 /tmp/fat32.0`
+- `mkdir /tmp/fat32.0/0000`
+- `sudo cp $HUDK_PATH/example/data/hudk.dat /tmp/fat32.0/0000/`
+- `sudo cp $HUDK_PATH/example/data/hudk.pal /tmp/fat32.0/0000/`
+- `sudo mkdir /tmp/fat32.0/0001`
+- `sudo cp $HUDK_PATH/example/data/hudson.dat /tmp/fat32.0/0001/`
+- `sudo cp $HUDK_PATH/example/data/hudson.pal /tmp/fat32.0/0001/`
+- `sync`
+- `sudo umount /tmp/fat32.0`
+- `sudo losetup -d /dev/loop0`
+
+#### Extract non-empty sectors and generate assembly file
+`img_slice` is a simple C program that will append every-non empty sectors in 8kB files and output an asm file listing the used sectors and data files.
+Simply run
+- `gcc -Wall img_slice && ./a.out fat32.img`
+
+If you generated the `fat32.img` as specified above you'll get the following files:
+ - bank_0000
+ - bank_0001
+ - bank_0002
+ - fat32.img.inc
+
+The last file will contains the list of "used" sectors, bank files and the total number of sectors and "used" sectors.
+```
+sectors:
+    .dw $0000,$0800,$0801,$0806,$0820,$0a87,$0cee,$0cef
+    .dw $0cf0,$0cf1,$0cf2,$0cf3,$0cf4,$0cf5,$0cf6,$0cf7
+    .dw $0cf8,$0cf9,$0cfa,$0cfb,$0cfc,$0cfd,$0cfe,$0cff
+    .dw $0d00,$0d01,$0d02,$0d03,$0d04,$0d05,$0d06,$0d07
+    .dw $0d08,$0d09,$0d0a
+
+    .bank $02
+    .org  $4000
+bank_0000:
+    .incbin "bank_0000"
+
+    .bank $03
+    .org  $4000
+bank_0001:
+    .incbin "bank_0001"
+
+sector_count = 81920
+sector_used = 35
+```
---
+++ example/data/fat32/img_slice.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <stdint.h>
+
+int slice_sectors(uint8_t *buffer, size_t size, const char *prefix) {
+    int ret = EXIT_SUCCESS;
+    char filename[256];
+    
+    snprintf(filename, sizeof(filename), "%s.inc", prefix);
+    FILE *info = fopen(filename, "wb");
+
+    uint8_t bank[8192];
+    size_t count = 0;
+    size_t k = 0;
+    
+    size_t used = 0;
+    size_t sectors = 0;
+    
+    size_t i, j;
+    fprintf(info, "sectors:");
+    for(i=0; i<size; i+=512, sectors++) {
+        uint8_t *ptr = buffer + i;
+        size_t sector_size = ((i+512) <= size) ? 512 : (size-i);
+        for(j=0; (j<sector_size) && (ptr[j]==0); j++) {
+        }
+
+        if(j < sector_size) {
+            fprintf(info, "%s$%04zx", (used%8) ? "," : "\n    .dw ", i/512);
+            
+            memcpy(bank+k, buffer+i, sector_size);
+            k += sector_size;
+            if(k >= 8192) {
+                snprintf(filename, 256, "bank_%04zx", count);
+                
+                FILE *out = fopen(filename, "wb");
+                fwrite(bank, 1, 8192, out);
+                fclose(out);
+
+                count++;                
+                k = 0;
+            }            
+            used++;
+        }
+    }
+    fprintf(info, "\n\n");
+    
+    if(k) {
+        snprintf(filename, 256, "bank_%04zx", count);
+        FILE *out = fopen(filename, "wb");
+        fwrite(bank, 1, k, out);
+        fclose(out);
+    }    
+    
+    j = 2;
+    for(i=0; i<count; i++) {
+        snprintf(filename, 256, "bank_%04zx", i);    
+        fprintf(info,
+                "    .bank $%02zx\n"
+                "    .org  $4000\n"
+                "%s:\n    .incbin \"%s\"\n\n"
+                , i+j
+                , filename
+                , filename);
+    }
+    fprintf(info, "sector_count = %zd\n", sectors);
+    fprintf(info, "sector_used = %zd\n", used);
+    fclose(info);
+    
+    return ret;
+}
+
+int main(int argc, char **argv) {
+    if(argc != 2) {
+        fprintf(stderr, "Usage: img_slice input\n");
+        return EXIT_FAILURE;
+    }
+    
+    size_t size;
+    uint8_t *buffer;
+    FILE *in;
+    
+    in = fopen(argv[1], "rb");
+    if(in == NULL) {
+        fprintf(stderr, "Failed to open %s : %s \n", argv[1], strerror(errno));
+        return EXIT_FAILURE;
+    }
+    
+    fseek(in, 0, SEEK_END);
+    size = ftell(in);
+    fseek(in, 0, SEEK_SET);
+    size -= ftell(in);
+    
+    int ret = EXIT_SUCCESS;
+    
+    buffer = (uint8_t*)malloc(size);
+    if(fread(buffer, 1, size, in) != size) {
+        fprintf(stderr, "Failed to read %zd bytes from %s : %s \n", size, argv[1], strerror(errno));
+        ret = EXIT_FAILURE;
+    }
+    fclose(in);
+    
+    if(ret == EXIT_SUCCESS) {
+        ret = slice_sectors(buffer, size, argv[1]);
+    }
+    
+    free(buffer);
+    return ret;
+}
