c83c4d7144fc1d796fc9f9e7c4dc6b8bb0ed2bef
MooZ <mooz@blockos.org>
2019-11-27 00:08:34 +0100

    Reworked aabb/aabb intersection test

--- include/sprite.s
+++ include/sprite.s
@@ -6,10 +6,7 @@
 ;;
 
     .zp
-_xmin .ds 4
-_ymin .ds 4
-_xmax .ds 4
-_ymax .ds 4
+_sprite.tmp .ds 2
 
     .bss
 sprite.attribute .ds 128
@@ -31,145 +28,103 @@ sprite.shape     .ds 256
 ; [todo] disc/disc intersection test
 ;       sqr(x0-x1) + sqr(y0-y1) <= sqr(r0+r1)
 
-init_q0:
-    lda    sprite.x, X
-    sta    <_xmin
-
-    lda    sprite.x+64, X
-    sta    <_xmin+1
-
-    lda    sprite.y, X
-    sta    <_ymin
-
-    lda    sprite.y+64, X
-    sta    <_ymin+1
-
-    ; x_max[0]
-    lda    sprite.shape+64, X
-    clc
-    adc    <_xmin
-    sta    <_xmax
-    cla
-    adc    <_xmin+1
-    sta    <_xmax+1
-
-    ; x_min[0]
-    lda    sprite.shape, X
-    and    #$7f
-    clc
-    adc    <_xmin
-    sta    <_xmin
-    cla
-    adc    <_xmin+1
-    sta    <_xmin+1
-
-    ; y_max[0]
-    lda    sprite.shape+192, X
-    clc
-    adc    <_ymin
-    sta    <_ymax
-    cla
-    adc    <_ymin+1
-    sta    <_ymax+1
-
-    ; y_min[0]
-    lda    sprite.shape+128, X
-    clc
-    adc    <_ymin
-    sta    <_ymin
-    cla
-    adc    <_ymin+1
-    sta    <_ymin+1
-
-    rts
-
-init_q1:
-    lda    sprite.x, Y
-    sta    <_xmin+2
-
-    lda    sprite.x+64, Y
-    sta    <_xmin+3
-
-    lda    sprite.y, Y
-    sta    <_ymin+2
-
-    lda    sprite.y+64, Y
-    sta    <_ymin+3
-
-    ; x_max[1]
-    lda    sprite.shape+64, Y
-    clc
-    adc    <_xmin+2
-    sta    <_xmax+2
-    cla
-    adc    <_xmin+3
-    sta    <_xmax+3
-
-    ; x_min[1]
+intersect_q0q1:
+    ; x_min1
     lda    sprite.shape, Y
     and    #$7f
     clc
-    adc    <_xmin+2
-    sta    <_xmin+2
+    adc    sprite.x, Y
+    sta    <_sprite.tmp
     cla
-    adc    <_xmin+3
-    sta    <_xmin+3
+    adc    sprite.x+64, Y
+    sta    <_sprite.tmp+1
 
-    ; y_max[1]
-    lda    sprite.shape+192, Y
+    ; x_max0
+    lda    sprite.shape+64, X
     clc
-    adc    <_ymin+2
-    sta    <_ymax+2
+    adc    sprite.x, X
+    pha
     cla
-    adc    <_ymin+3
-    sta    <_ymax+3
-
-    ; y_min[1]
-    lda    sprite.shape+128, Y
-    clc
-    adc    <_ymin+2
-    sta    <_ymin+2
-    cla
-    adc    <_ymin+3
-    sta    <_ymin+3
-
-    rts
-
-intersect_q0q1:
+    adc    sprite.x+64, X
     ; xmax0 < xmin1 => NOK
-    lda    <_xmax+1
-    cmp    <_xmin+3
+    cmp    <_sprite.tmp+1
     bcc    @nop
     bne    @l0
-    lda    <_xmax
-    cmp    <_xmin+2
+    pla
+    cmp    <_sprite.tmp
     bcc    @nop
 @l0:
+    ; x_min0
+    lda    sprite.shape, X
+    and    #$7f
+    clc
+    adc    sprite.x, X
+    sta    <_sprite.tmp
+    cla
+    adc    sprite.x+64, X
+    sta    <_sprite.tmp+1
+
+    ; x_max1
+    lda    sprite.shape+64, Y
+    clc
+    adc    sprite.x, Y
+    pha
+    cla
+    adc    sprite.x+64, Y
     ; xmax1 < xmin0 => NOK
-    lda    <_xmax+3
-    cmp    <_xmin+1
+    cmp    <_sprite.tmp+1
     bcc    @nop
     bne    @l1
-    lda    <_xmax+2
-    cmp    <_xmin
+    pla
+    cmp    <_sprite.tmp
     bcc    @nop
-@l1:    
+@l1:
+    ; y_min1
+    lda    sprite.shape+192, Y
+    clc
+    adc    sprite.y, Y
+    sta    <_sprite.tmp
+    cla
+    adc    sprite.y+64, Y
+    sta    <_sprite.tmp+1
+
+    ; y_max0
+    lda    sprite.shape+192, X
+    clc
+    adc    sprite.y, X
+    pha
+    cla
+    adc    sprite.y+64, X
     ; ymax0 < ymin1 => NOK
-    lda    <_ymax+1
-    cmp    <_ymin+3
+    cmp    <_sprite.tmp+1
     bcc    @nop
     bne    @l2
-    lda    <_ymax
-    cmp    <_ymin+2
+    pla
+    cmp    <_sprite.tmp
     bcc    @nop
 @l2:
+    ; y_min0
+    lda    sprite.shape+128, X
+    clc
+    adc    sprite.y, X
+    sta    <_sprite.tmp
+    cla
+    adc    sprite.y+64, X
+    sta    <_sprite.tmp+1
+
+    ; ymax1
+    lda    sprite.shape+192, Y
+    clc
+    adc    sprite.y, Y
+    pha
+    cla
+    adc    sprite.y+64, Y
     ; ymax1 < ymin0 => NOK
-    lda    <_ymax+3
-    cmp    <_ymin+1
+    cmp    <_sprite.tmp+1
     bcc    @nop
     bne    @l3
-    lda    <_ymax+2
-    cmp    <_ymin
+    pla
+    cmp    <_sprite.tmp
     bcc    @nop
 @l3:
     rts
